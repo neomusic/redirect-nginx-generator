cb899c9b33f4c1668cea707933e5d3170e8bd2ef
Giuseppe Aremare <g.aremare@palmabit.com>
2019-02-14 11:21:36 +0100

    add dist

---
+++ dist/cli.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var _commander = _interopRequireDefault(require("commander"));
+
+var _csvToRedirect = _interopRequireDefault(require("./csvToRedirect"));
+
+var _package = require("./../package.json");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_commander.default.version(_package.version).arguments('<csvfile> <out>').action(function (csvfile, out) {
+  (0, _csvToRedirect.default)(csvfile, out);
+});
+
+_commander.default.parse(process.argv);
---
+++ dist/csvToRedirect.js
@@ -0,0 +1,43 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _csvtojson = _interopRequireDefault(require("csvtojson"));
+
+var _path = require("path");
+
+var _fs = require("fs");
+
+require("colors");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _default = function _default(csvfile, out) {
+  (0, _csvtojson.default)({
+    noheader: true,
+    delimiter: ';',
+    output: "csv"
+  }).fromFile((0, _path.resolve)(csvfile)).then(function (jsonObj) {
+    return jsonObj.map(function (row) {
+      var start = row[0];
+      var go = row[1];
+      return "rewrite ^".concat(start, "(/$|$) ").concat(go, " permanent;");
+    });
+  }).then(function (res) {
+    (0, _fs.writeFile)((0, _path.resolve)(out), res.join('\n'), function (err) {
+      if (err) {
+        throw err;
+      }
+    });
+  }).then(function () {
+    console.log('All done \n Check the file at'.green.bold);
+    console.log("".concat((0, _path.resolve)(out)).bold);
+  }).catch(function (err) {
+    return console.error('Something went wrong'.red.bold, err);
+  });
+};
+
+exports.default = _default;
